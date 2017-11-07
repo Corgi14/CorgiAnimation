b268827154a5e9b9e1d9502c16504c6cfa6d0624
XiangYe <XiangYeGit@outlook.com>
2017-11-07 18:22:31 +0800

    Build Product

---
+++ CorgiAnimation/CorgiAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CorgiAnimation
+//
+//  Created by XiangYe on 2017/11/7.
+//  Copyright © 2017年 Corgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
